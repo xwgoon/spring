230710cfc6bbb102771308fd15123e772be8b7ec
xw <1178124579@qq.com>
2019-02-28 20:01:17 +0800

    websocket

--- source/web/src/main/java/com/myapp/web/websocket/MyHandshakeInterceptor.java
+++ source/web/src/main/java/com/myapp/web/websocket/MyHandshakeInterceptor.java
@@ -2,6 +2,7 @@ package com.myapp.web.websocket;
 
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
@@ -12,6 +13,8 @@ public class MyHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
         System.out.println("调用beforeHandshake");
+        String userId = ((ServletServerHttpRequest) request).getServletRequest().getParameter("userId");
+        attributes.put("userId", Long.valueOf(userId));
         return super.beforeHandshake(request, response, wsHandler, attributes);
     }
 
--- source/web/src/main/java/com/myapp/web/websocket/MyWebSocketHandler.java
+++ source/web/src/main/java/com/myapp/web/websocket/MyWebSocketHandler.java
@@ -12,13 +12,15 @@ import java.util.Map;
 
 public class MyWebSocketHandler extends TextWebSocketHandler {
 
-    private static final Map<String, WebSocketSession> ONLINE_USERS = new HashMap<>();
+    private static final Map<Long, WebSocketSession> ONLINE_USERS = new HashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("调用afterConnectionEstablished");
-        System.out.println("用户【" + session.getId() + "】连接成功");
-        session.sendMessage(new TextMessage("用户【" + session.getId() + "】你好，我是服务器"));
+        System.out.println(session.getAttributes());
+        Long userId = (Long) session.getAttributes().get("userId");
+        System.out.println("用户【" + userId + "】连接成功");
+        session.sendMessage(new TextMessage("用户【" + userId + "】你好，我是服务器"));
 //        new Thread(() -> {
 //            while (true) {
 //                try {
@@ -30,22 +32,23 @@ public class MyWebSocketHandler extends TextWebSocketHandler {
 //                }
 //            }
 //        }).start();
-        ONLINE_USERS.put(session.getId(), session);
+        ONLINE_USERS.put(userId, session);
     }
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
         System.out.println("调用handleMessage");
-        System.out.println("用户【" + session.getId() + "】发送消息：" + message.getPayload());
+        Long userId = (Long) session.getAttributes().get("userId");
+        System.out.println("用户【" + userId + "】发送消息：" + message.getPayload());
 
         JSONObject jsonObject = JSONObject.fromObject(message.getPayload().toString());
-        String toSessionId = jsonObject.getString("to");
-        WebSocketSession toUser = ONLINE_USERS.get(toSessionId);
-        if (toUser != null && toUser.isOpen()) {
+        Long toUserId = jsonObject.getLong("to");
+        WebSocketSession toUserSession = ONLINE_USERS.get(toUserId);
+        if (toUserSession != null && toUserSession.isOpen()) {
             JSONObject result = new JSONObject();
-            result.put("from", session.getId());
+            result.put("from", userId);
             result.put("msg", jsonObject.getString("msg"));
-            toUser.sendMessage(new TextMessage(result.toString()));
+            toUserSession.sendMessage(new TextMessage(result.toString()));
         }
 
     }
@@ -58,8 +61,9 @@ public class MyWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
         System.out.println("调用afterConnectionClosed");
-        System.out.println("用户【" + session.getId() + "】连接关闭");
-        ONLINE_USERS.remove(session.getId());
+        Long userId = (Long) session.getAttributes().get("userId");
+        System.out.println("用户【" + userId + "】连接关闭");
+        ONLINE_USERS.remove(userId);
     }
 
     public static void main(String[] args) {
