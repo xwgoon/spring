f582a6ad23bce446756c9c595a9817b4bc7e4bf5
xw <1178124579@qq.com>
2020-11-02 19:28:36 +0800

    读取CSV

---
+++ source/service/src/main/java/com/myapp/service/util/csv/CSVTest.java
@@ -0,0 +1,60 @@
+package com.myapp.service.util.csv;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class CSVTest {
+
+    public static void main(String[] args) {
+        simpleRead();
+
+        System.out.println("-----");
+
+        openCSVRead();
+
+        System.out.println("-----");
+
+        String[] arr = {"a", "b"};
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void openCSVRead() {
+        String csvFile = "E:\\project\\spring\\source\\service\\src\\main\\java\\com\\myapp\\service\\util\\csv\\test1.csv";
+        String[] line;
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))){
+//            reader.skip(1);
+            while ((line = reader.readNext()) != null) {
+                System.out.println(Arrays.toString(line));
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 有局限，不能正确解析逗号（,）和双引号（""），并且会把最外层的双引号解析为值的一部分
+     */
+    private static void simpleRead() {
+        String csvFile = "E:\\project\\spring\\source\\service\\src\\main\\java\\com\\myapp\\service\\util\\csv\\test1.csv";
+        String line;
+        String csvSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
+                String[] arr = line.split(csvSplitBy);
+                System.out.println(Arrays.toString(arr));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
