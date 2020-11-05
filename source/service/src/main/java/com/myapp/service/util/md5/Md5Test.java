7c92ca3977da7ec06b03ce63716b9636594e2477
xw <1178124579@qq.com>
2020-11-05 19:48:28 +0800

    MD5

---
+++ source/service/src/main/java/com/myapp/service/util/md5/Md5Test.java
@@ -0,0 +1,54 @@
+package com.myapp.service.util.md5;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5Test {
+
+    public static void main(String[] args) {
+        String str = "ILoveJava";
+        String result = "35454B055CC325EA1AF2126E27707052";
+//        String hash = md5(str);
+        String hash = md5(new File("E:\\project\\spring\\source\\service\\src\\main\\java\\com\\myapp\\service\\util\\md5\\test.txt"));
+        System.out.println(hash);
+        String hash2 = md5(new File("E:\\project\\spring\\source\\service\\src\\main\\java\\com\\myapp\\service\\util\\md5\\test2.txt"));
+        System.out.println(hash2);
+//        System.out.println(result.equals(hash));
+
+    }
+
+    private static String md5(String str) {
+        return md5(str.getBytes());
+    }
+
+    private static String md5(File file) {
+        try {
+            return md5(Files.readAllBytes(file.toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String md5(byte[] bytes) {
+        //一、jdk方式
+//        try {
+//            MessageDigest md = MessageDigest.getInstance("MD5");
+//            md.update(bytes);
+//            byte[] digest = md.digest();
+//            return DatatypeConverter.printHexBinary(digest).toUpperCase();
+//        } catch (NoSuchAlgorithmException e) {
+//            throw new IllegalArgumentException(e);
+//        }
+
+        //二、Apache Commons方式
+        return DigestUtils.md5Hex(bytes).toUpperCase();
+    }
+
+
+}
